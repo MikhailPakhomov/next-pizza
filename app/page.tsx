9e9beec7877f7d26074d1b4ca9500e64f926ce3c
Михаил Пахомов <mihail93@mail.ru>
2024-10-14 15:38:11 +0300

    feat: добавлена разметка Топ-бара

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+import { Container, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
     <>
-      <h1>Главная</h1>
+      <Container className="mt-10">
+        <Title text={"Все пиццы"} size="lg" className="font-extrabold" />
+      </Container>
+      <TopBar />
+      <div style={{ height: "3000px" }}></div>
     </>
   );
 }
